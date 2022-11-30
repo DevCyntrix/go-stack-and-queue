22e38dfb07abec26136aaf070c27ef2738f5f9da
DevCyntrix <devcyntrix@hotmail.com>
2022-11-30 12:59:43 +0100

    Rename the index to readerIndex

--- stack/arraystack.go
+++ stack/arraystack.go
@@ -2,22 +2,22 @@ package stack
 
 // LIFO (Last in First out)
 type ArrayStack struct {
-	index int
-	array []interface{}
+	readerIndex int
+	array       []interface{}
 }
 
 // Creates a new Stack with a constant size
 func NewArrayStack(size int32) *ArrayStack {
-	return &ArrayStack{array: make([]interface{}, size)} // This slice has a capacity of size
+	return &ArrayStack{readerIndex: 0, array: make([]interface{}, size)} // This slice has a capacity of size
 }
 
 // Pushs the element on the top of the stack, otherwise it returns the stack is full error
 func (stack *ArrayStack) Push(value interface{}) error {
-	if stack.index >= len(stack.array) {
+	if stack.readerIndex >= len(stack.array) {
 		return ErrStackIsFull
 	}
-	stack.array[stack.index] = value
-	stack.index++
+	stack.array[stack.readerIndex] = value
+	stack.readerIndex++
 	return nil
 }
 
@@ -26,8 +26,8 @@ func (stack *ArrayStack) Pop() (interface{}, error) {
 	if stack.IsEmpty() {
 		return nil, ErrStackIsEmpty
 	}
-	stack.index--
-	return stack.array[stack.index], nil
+	stack.readerIndex--
+	return stack.array[stack.readerIndex], nil
 }
 
 // Returns the element on the top of the stack
@@ -35,11 +35,11 @@ func (stack *ArrayStack) Top() (interface{}, error) {
 	if stack.IsEmpty() {
 		return nil, ErrStackIsEmpty
 	}
-	i := stack.index - 1
+	i := stack.readerIndex - 1
 	return stack.array[i], nil
 }
 
 // Returns true if the stack has no elements
 func (stack *ArrayStack) IsEmpty() bool {
-	return stack.index <= 0
+	return stack.readerIndex <= 0
 }
